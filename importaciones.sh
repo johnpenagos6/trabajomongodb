8a4d280e9e9ab450fe462c69ad3a60bd660808c2
john.penagos.contractor <john.penagos.contractor@bbva.com>
2024-05-26 02:38:01 -0500

    subida de archivos actividad 2

---
+++ importaciones.sh
@@ -0,0 +1,5 @@
+mongoimport --db torneobaloncesto1vs1 --collection equipos --file equipos.json --jsonArray
+mongoimport --db torneobaloncesto1vs1 --collection deportistas --file deportistas.json --jsonArray
+mongoimport --db torneobaloncesto1vs1 --collection arbitros --file arbitros.json --jsonArray
+mongoimport --db torneobaloncesto1vs1 --collection encuentros --file encuentros.json --jsonArray
+mongoimport --db torneobaloncesto1vs1 --collection tablaposiciones --file tablaposiciones.json --jsonArray
